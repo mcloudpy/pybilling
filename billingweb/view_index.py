0435f29414e4eea57cf5d9806389a590f2dbb097
Luis Rodriguez <luis.rodriguezgil@deusto.es>
2014-09-25 17:40:23 +0200

    Improved navigation

--- billingweb/view_index.py
+++ billingweb/view_index.py
@@ -2,9 +2,11 @@
 
 # Mostly for debugging purposes, this snippet will print the site-map so that we can check
 # which methods we are routing.
-from flask import render_template
+from flask import render_template, redirect, url_for
 from markupsafe._speedups import escape
 from billingweb import flask_app
+
+import os
 
 
 @flask_app.route("/site-map")
@@ -20,6 +22,16 @@ def site_map():
 
 @flask_app.route("/")
 def index():
+    """
+    Index page.
+    """
+
+    # Check if the Web has been installed.
+
+    # TODO: THIS CHECK IS TEMPORARY. UPDATE IT TO SUPPORT MORE DBS ETC.
+    if not os.path.isfile(flask_app.config["DATABASE_URI"].split(":///", 1)[1]):
+        return redirect(url_for("install"))
+
     return render_template("index.html")
 
 
